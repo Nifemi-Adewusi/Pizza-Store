bc82c319996a32c47d744dc6b09ff2b4fd9c6198
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-06 08:16:09 +0100

    Include App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
   {
     path: "/menu",
     element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
   },
 ]);
 function App() {
